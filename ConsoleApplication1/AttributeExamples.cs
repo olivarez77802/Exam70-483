ba032b12a25e98e9f670a11b92339a93bac9bb7a
Jesse-Olivarez <Jesse-Olivarez@tamus.edu>
2018-07-03 12:54:20 -0500

    Work - Attribute comments

--- ConsoleApplication1/AttributeExamples.cs
+++ ConsoleApplication1/AttributeExamples.cs
@@ -4,45 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exam70483
+namespace ConsoleApplication1
 {
-    public class RunTimeTypes
+    class AttributeExamples
     {
-        /*
-           
-           Attributes allow you to add declarative information to your programs.  This information can be queried at run time using
-           reflection.  
-           
-           There are several predefined attributes created by .NET It is also possible to create your own Custom Attributes.
-           A few predefined attributes within the .Net framework:
-           obsolete - Marks type and type members as outdated
-           WebMethod - To expose a webmethod as an XML Web Service
-           Serializable - Indicates that a class can be serializable
-
-           An attribute is a class that inherits from the System.Attributes namespace
-           https://www.youtube.com/watch?v=TWZXj27-6Cw&list=PLAC325451207E3105&index=52   
-           
-           See also DynamicExamples.cs
-           
-           Windows RunTime
-           https://www.youtube.com/watch?v=24Uaf3Y4qW8
-               
-                         
-        */
-
 
         public static void Menu()
         {
-
             int x = 0;
             do
             {
                 Console.Clear();
-                Console.WriteLine(" Find, execute, and create types at runtime by using reflection");
+                Console.WriteLine("Atributes");
                 Console.WriteLine("    Create and apply attributes; read attributes; generate code at runtime");
                 Console.WriteLine("    using CodeDom and Lambda expressions; use types from the System.Refection");
                 Console.WriteLine("    namespace, including Assembly, PropetryInfo, MethodInfo, Type");
-          
+
                 Console.WriteLine(" 0.  Reflection ");
                 Console.WriteLine(" 1.  Relection - GetTypes");
                 Console.WriteLine(" 2.  Refelection - GetExecutingAssembly");
@@ -71,10 +48,10 @@ namespace Exam70483
                         AssemblyClassExamples.Determine_type();
                         break;
                     case 5:
-                         AttributeExamples.Menu();
+                        AttributeExamples.Menu();
                     case 6:
                         CodeDom_Example();
-                         break;
+                        break;
                     case 9:
                         x = 9;
                         break;
@@ -86,15 +63,6 @@ namespace Exam70483
 
 
             } while (x < 9);
-
-        }
-        static void CodeDom_Example()
-        {
-            /*
-             * Code (D)ocument (O)bject (M)odel
-             * Writing Code that Writes Code
-             * 
-             */
         }
     }
 }
