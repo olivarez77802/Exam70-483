21dffcfbc3babbc647e77ceeedfaced57c753513
Jesse Olivarez <olivarez77802@gmail.com>
2019-02-17 21:45:36 -0600

    Debug comments

--- ConsoleApplication1/DebugApplication.cs
+++ ConsoleApplication1/DebugApplication.cs
@@ -1,22 +1,16 @@
-﻿using System;
+﻿#define OldMethod
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Exam70483
 {
-    /*
-     *  A method can use Assert to verify that its data makes sense.   For example
-     *  Debug.Assert(items.Length <=100)
-     *  If an order contains more that 100 items, the Assert statement halts
-     *  execution, so you can examine the code to decide whether this is a bug
-     *  or just an unusual order.
-     *  
-     *  In a "release" build, the above Assert statement is ignored.
-     * 
-     */ 
+   
     class DebugApplication
     {
         public static void Menu()
@@ -30,6 +24,8 @@ namespace Exam70483
                 Console.WriteLine(" 0.  Create and Manage Preprocessor directives \n ");
                 Console.WriteLine(" 1.  Choose an approriate build type \n ");
                 Console.WriteLine(" 2.  Manage program database files (debug symbols) \n ");
+                Console.WriteLine(" 3.  Debug.Assert");
+                Console.WriteLine(" 4.  Trace File");
                 Console.WriteLine(" 9.  Quit            \n\n ");
 
                 int selection;
@@ -38,10 +34,16 @@ namespace Exam70483
                 {
                     case 0:
                         PreProcessorDirectives();
+                        Console.ReadKey();
                         break;
                     case 1:
                         break;
                     case 2:
+                        break;
+                    case 3: DebugAssert();
+                        break;
+                    case 4: Trace_File();
+                        Console.ReadKey();
                         break;
                     case 9:
                         x = 9;
@@ -56,12 +58,12 @@ namespace Exam70483
         }
         static void PreProcessorDirectives()
         {
-            /*  The #define directive defines a preprocessor symbor or conditional compilation symbol for the module
+            /*  The #define directive defines a preprocessor symbol or conditional compilation symbol for the module
              *  that contains the directive.  You can use the #if and #elif proprocessor directive to see if the sybmol
              *  is defined.  Note: You cannot assign a value to the symbol, so it isn't comparable to a C# variable or
              *  constant.  All you can do is define or undefine the symbol and see if it has been defined.
              *  
-             *  Both the #deine and #undef directives must appear in a module before any nondirective statemement in
+             *  Both the #define and #undef directives must appear in a module before any nondirective statemement in
              *  a modules.  Because these statements must go at the beginning of the file, you normally don't use
              *  #undef to undefine a symbol that you had just created with a #define.  Usually #undef is more useful
              *  for undefining a symbol that you created using the projects' property pages.
@@ -72,7 +74,7 @@ namespace Exam70483
              *  
              *  Top of program
              *  #define DEBUG1
-             *  //#define DEGUG1
+             *  //#define DEGUG2
              *  
              *       private void VerifyInternetConnections
              *       {
@@ -112,12 +114,54 @@ namespace Exam70483
             Process.Start("http://msdn.microsoft.com/en-us/library/vstudio/ed8yd1ha%28v=vs.100%29.aspx");
 #if DEMO
             Console.WriteLine("Line only executed in Demo mode");
-#elif DEMO
+#elif !DEMO
             Console.WriteLine("Line only executed in NON-Demo mode");
 #endif
 
-        }
+#if OldMethod
+#warning Using obsolete method
+#else
+#warning testing
+#endif
 
+
+
+
+        }
+        static void DebugAssert()
+        {
+   /*
+    *  A method can use Assert to verify that its data makes sense.   For example
+    *  Debug.Assert(items.Length <=100)
+    *  If an order contains more that 100 items, the Assert statement halts
+    *  execution, so you can examine the code to decide whether this is a bug
+    *  or just an unusual order.
+    *  
+    *  The "Assert" statement executes only in debug releases of a program.
+    *  In a "release" build, the above Assert statement is ignored.
+    * 
+    */
+
+        }
+        static void Trace_File()
+        {
+            // Create the trace output file
+            // Stored in C:\Users\olivarez77802\Documents\Visual Studio 2015\Projects\GitRemoteRepositories\ConsoleApplication1\ConsoleApplication1\bin\Debug
+            Stream traceStream = File.Create("TraceFile.txt");
+            //Alternative - to append new text at end of file
+            // Stream traceStream = File.Open("TraceFile.txt",
+            //        FileMode.Append, FileAccess.Write, FileShare.Read);
+            //
+
+            // Create a TextWriterTraceListener for the trace output file.
+            TextWriterTraceListener traceListener =
+                new TextWriterTraceListener(traceStream);
+            Trace.Listeners.Add(traceListener);
+
+            //write a startup note into the trace file
+            Trace.WriteLine("Trace started " + DateTime.Now.ToString());
+            Trace.Flush();
+        }
 
     }
 }
